f8c8dc14af2794b37c8d42f5a454608886e97aa2
rajxode <rajsh1202@gmail.com>
2023-08-21 16:48:48 +0530

    added comments to all the redux files

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -2,6 +2,7 @@
 // for creating store
 import { configureStore } from "@reduxjs/toolkit";
 
+// importing the contact Reducers
 import { contactReducer } from "../Reducers/contactReducer";
 
 // creating store from reducers
